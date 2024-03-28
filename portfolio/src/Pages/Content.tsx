ac55d3781da72b3f932d16e5bcae888c067f5253
Blake Jones <blakeejoness@gmail.com>
2024-03-28 04:50:38 +0000

    Add Content component and remove Home component

---
+++ portfolio/src/Pages/Content.tsx
@@ -0,0 +1,90 @@
+import { useTheme } from "@emotion/react";
+import { Card, CardContent, CardHeader, Grid, Typography } from "@mui/material";
+
+export default function Content() {
+  const theme = useTheme();
+  return (
+    <>
+      <Grid container>
+        <Grid item xs={6} px={6}>
+          <Card
+            sx={{
+              backgroundColor: "secondary.main",
+              color: "white",
+              height: "100%",
+              border: `2px ridge ${theme.palette.primary.main}`,
+            }}
+          >
+            <CardHeader
+              title="About Me"
+              sx={{
+                backgroundColor: theme.palette.primary.main,
+                color: theme.palette.secondary.contrastText,
+                backgroundImage: `linear-gradient(45deg, ${theme.palette.primary.main}, ${theme.palette.secondary.main})`,
+                border: `1px solid ${theme.palette.secondary.main}`,
+                borderRadius: "5px",
+                boxShadow: `0 0 5px ${theme.palette.secondary.main}`,
+                textShadow: "none",
+              }}
+            />
+            <CardContent>
+              <Typography variant="body1">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea
+                minus eveniet repellat vel beatae accusamus explicabo quaerat
+                asperiores debitis totam obcaecati consectetur eaque doloribus
+                adipisci neque iure distinctio, tenetur iusto? Lorem ipsum,
+                dolor sit amet consectetur adipisicing elit. Ea minus eveniet
+                repellat vel beatae accusamus explicabo quaerat asperiores
+                debitis totam obcaecati consectetur eaque doloribus adipisci
+                neque iure distinctio, tenetur iusto? Lorem ipsum, dolor sit
+                amet consectetur adipisicing elit. Ea minus eveniet repellat vel
+                beatae accusamus explicabo quaerat asperiores debitis totam
+                obcaecati consectetur eaque doloribus adipisci neque iure
+                distinctio, tenetur iusto?
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={6} px={6}>
+          <Card
+            sx={{
+              backgroundColor: "secondary.main",
+              color: "white",
+              height: "100%",
+              border: `2px ridge ${theme.palette.primary.main}`,
+            }}
+          >
+            <CardHeader
+              title="Experience & Skills"
+              sx={{
+                backgroundColor: theme.palette.primary.main,
+                color: theme.palette.secondary.contrastText,
+                backgroundImage: `linear-gradient(45deg, ${theme.palette.primary.main}, ${theme.palette.secondary.main})`,
+                border: `1px solid ${theme.palette.secondary.main}`,
+                borderRadius: "5px",
+                boxShadow: `0 0 5px ${theme.palette.secondary.main}`,
+                textShadow: "none",
+              }}
+            />
+            <CardContent>
+              <Typography variant="body1">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea
+                minus eveniet repellat vel beatae accusamus explicabo quaerat
+                asperiores debitis totam obcaecati consectetur eaque doloribus
+                adipisci neque iure distinctio, tenetur iusto? Lorem ipsum,
+                dolor sit amet consectetur adipisicing elit. Ea minus eveniet
+                repellat vel beatae accusamus explicabo quaerat asperiores
+                debitis totam obcaecati consectetur eaque doloribus adipisci
+                neque iure distinctio, tenetur iusto? Lorem ipsum, dolor sit
+                amet consectetur adipisicing elit. Ea minus eveniet repellat vel
+                beatae accusamus explicabo quaerat asperiores debitis totam
+                obcaecati consectetur eaque doloribus adipisci neque iure
+                distinctio, tenetur iusto?
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </>
+  );
+}
