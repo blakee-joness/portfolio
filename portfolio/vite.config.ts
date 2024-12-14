2c79d7befc32d39d238d9c8cda0f221f463d81f9
Blake Jones <82840474+blakee-joness@users.noreply.github.com>
2024-12-14 08:31:35 +0000

    Configure Vite server to run on port 5173 and bind to ipv4 localhost

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -10,4 +10,9 @@ export default defineConfig({
     host: true,
     port: 8080,
   },
+  server: {
+    // https://github.com/vitejs/vite/issues/16522#issuecomment-2432846922
+    port: 5173,
+    host: "127.0.0.1",
+  },
 });
